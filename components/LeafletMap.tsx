dcf4c96e67647c05711f3bd8d9bee36dd03dcec5
IAT <israel24961@hotmail.com>
2023-08-03 22:42:47 +0200

    Persistent database data (local storage)
    List of coordinates
    
    Bruh

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,15 +1,17 @@
 //@ts-nocheck
 "use client"
-import { useRef, useState, useMemo, StrictMode, useCallback } from 'react';
-import { Tooltip, useMap, MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import { useRef, useState, useMemo, useEffect, StrictMode, useCallback } from 'react';
+import { Tooltip, useMap, MapContainer, TileLayer, Marker, Popup, Circle, CircleMarker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet'
+import { CopyBlock, dracula } from "react-code-blocks"
+import Cookies from 'universal-cookie';
 
 function Coolbutton({ msgs, clicks }: { msgs: string[], clicks: any[] }) {
   let buttons = []
   for (let i = 0; i < msgs.length; i++) {
     buttons.push(
-      <button key={i} onClick={clicks[i]} className="group hover:scale-125 rounded-lg border-1 border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+      <button key={i} onClick={clicks[i]} className="group duration-500 hover:scale-125 rounded-lg border-1 border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-indigo-800/30">
         {msgs[i]}</button>)
   }
   // let map = useMap()
@@ -38,27 +40,15 @@ export default function LeafletMap({ dark, className }: { dark: boolean, classNa
   console.log("Leaf called")
   const position: [number, number] = [51.505, -0.09]; // Replace with your desired initial coordinates
   const leaf: L.Map = useRef(null)
-  const [AMarkers, setAMarkers] = useState(new Array)
+  const [AMarkers, setAMarkers] = useState(localStorage.getItem('bss')?JSON.parse(localStorage.getItem('bss')):[])
+  useEffect(() => {
+    localStorage.setItem('bss', JSON.stringify(AMarkers));
+  }, [AMarkers]);
+  const [showcircles, setshowcircles] = useState(false)
+  const add_bs = (np) => {
+    console.log(np ?? leaf.current.getCenter())
 
-  const add_c = () => {
-    console.log(leaf.current.getCenter())
-    setAMarkers(AMarkers.map(
-      (c, i) => {
-        if (i === AMarkers.length - 1) {
-          return { posi: c.posi, circle:1000 }
-        } else {
-          return c;
-        }
-      }
-    ));
-    AMarkers.forEach(element => {
-      console.log(element)
-    });
-  }
-  const add_bs = () => {
-    console.log(leaf.current.getCenter())
-
-    setAMarkers(AMarkers.concat([{ posi: leaf.current.getCenter() }]))
+    setAMarkers(AMarkers.concat([{ posi: leaf.current.getCenter(), circle: 1000 }]))
     AMarkers.forEach(element => {
       console.log(element)
     });
@@ -67,7 +57,7 @@ export default function LeafletMap({ dark, className }: { dark: boolean, classNa
     setAMarkers(AMarkers.map(
       (c, i) => {
         if (i === id) {
-          return { posi: pos }
+          return { posi: pos, circle: c.circle }
         } else {
           return c;
         }
@@ -78,12 +68,19 @@ export default function LeafletMap({ dark, className }: { dark: boolean, classNa
     setAMarkers(AMarkers.slice(0, AMarkers.length - 1));
   }
   const htmlAMarkers = AMarkers.map((obj, i) => {
-    if (obj.circle != null) {
-      return <CircleMarker key={i} posi={obj.posi} radius={obj.circle} callbck={() => "xd"} />
+    if (showcircles) {
+      return <Mycirclemarker key={i} posi={obj.posi} radius={obj.circle} callbck={(a) => { mod_PosMarker(i, a) }} map={leaf} />
     }
     else
       return <DraggableMarker key={i} posi={obj.posi} callbck={(p) => mod_PosMarker(i, p)} />
   })
+
+  let basesString = ""
+  for (let index = 0; index < AMarkers.length; index++) {
+    basesString += "\n"
+    basesString += JSON.stringify({ ...AMarkers[index].posi, radius: AMarkers[index].circle })
+
+  }
   const resp = (
     <StrictMode>
       <MapContainer className={className} ref={leaf} id="map" center={position} zoom={13} style={{ filter: dark ? 'invert(1)' : 'invert(0)', width: '100%', height: '60vh' }}>
@@ -93,7 +90,21 @@ export default function LeafletMap({ dark, className }: { dark: boolean, classNa
         />
         {htmlAMarkers}
       </MapContainer>
-      <Coolbutton msgs={["Add a base station", "Undo add", "Add coverage", "Reset base stations"]} clicks={[add_bs, mod_RemoveLast, add_c, go_to_madrid]} />
+      <Coolbutton msgs={["Add a base station", "Undo add", showcircles ? "Show markers" : "Show ranges",
+        "Reset base stations"]}
+        clicks={[add_bs, mod_RemoveLast, () => { setshowcircles((d) => !d) }, () => { setAMarkers(new Array()) }]} />
+      <div className={basesString ? "visible" : "collapse"}>
+        <CopyBlock
+          text={basesString}
+          language="json"
+          theme={dracula}
+          showLineNumbers={true}
+          startingLineNumber={0}
+        />
+      </div>
+
+
+
     </StrictMode>
   );
   return resp;
@@ -112,8 +123,33 @@ function NormalMarker({ posi, callbck }) {
   </Marker>;
 
 }
-function CircleMarker({ posi, radius, callbck }) {
-  return <Circle center={posi} pathOptions={{ color: 'red' }} radius={radius} />
+function Mycirclemarker({ posi, radius, callbck, map }) {
+  const [draggable, setdraggable] = useState(0)
+  const circ = useRef(null)
+
+  function update_pos(pos) {
+    console.log(circ);
+
+    circ.current.setLatLng(pos)
+  }
+  const eH = useMemo(
+    () => ({
+      mousemove(e) {
+        if (draggable) {
+          console.log("mousemove " + e.latlng)
+          update_pos(e.latlng)
+        }
+      },
+      click(e) {
+        setdraggable((d) => !d)
+        callbck(e.latlng)
+
+      }
+    }),
+    [draggable, callbck],
+  )
+  return (<Circle ref={circ} className="opacity-50 hover:opacity-100" eventHandlers={eH} center={posi} pathOptions={{ color: 'red' }} radius={radius} >
+  </Circle>)
 }
 function DraggableMarker({ posi, callbck }) {
 
@@ -147,7 +183,7 @@ function DraggableMarker({ posi, callbck }) {
   const [draggable, setDraggable] = useState(false)
   const markerRef = useRef(null)
   const toggleDraggable = useCallback((dra) => {
-    if (dra) {
+    if (curIcon === bigIcon) {
       setcurIcon(normalIcon)
     }
     else {
